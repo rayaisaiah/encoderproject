77276693d50dbbca76985fe4de95bc4f2f49e4ed
Isaiah Raya <rayaisaiah@gmail.com>
2016-10-11 13:40:32 -0700

    echo works!

--- encoder.js
+++ encoder.js
@@ -1,4 +1,33 @@
-$("#inputText").keydown(function(e) {
-  $('#textArea').append(document.getElementById("inputText").value);
+var echoTranslations = {
+65 : "a",
+66 : "b",
+67 : "c",
+68 : "d",
+69 : "e",
+70 : "f",
+71 : "g",
+72 : "h",
+73 : "i",
+74 : "j",
+75 : "k",
+76 : "l",
+77 : "m",
+78 : "n",
+79 : "o",
+80 : "p",
+81 : "q",
+82 : "r",
+83 : "s",
+84 : "t",
+85 : "u",
+86 : "v",
+87 : "w",
+88 : "x",
+89 : "y",
+90 : "z"
+}
 
+$("#inputText").keydown(function(e) {
+var answer = echoTranslations[e.keyCode];
+$('#textArea').append(answer);
 })
